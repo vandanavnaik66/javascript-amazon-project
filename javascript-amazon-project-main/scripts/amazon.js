aa11d68e021085c34d03e1f8c1e7ea0257cfecd1
vandanavnaik66 <vandanavnaik66@gmail.com>
2023-08-30 14:18:31 +0530

    feature of product adding to cart implemented

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,37 +1,6 @@
-        const products = [
-        {
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-            stars: 4.5,
-            count: 87,
-            },
-            priceCents: 1090,
-        },
-
-        {
-            image: "images/products/intermediate-composite-basketball.jpg",
-            name: "Intermediate Size Basketball",
-            rating: {
-            stars: 4,
-            count: 127,
-            },
-            priceCents: 2095,
-        },
-        {
-            image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-            name: "Adult Plain Cotton T-Shirt - 2 Pack",
-            rating: {
-            stars: 4,
-            count: 56,
-            },
-            priceCents: 7990,
-        },
-        ];
-
-        let productsHTML='';
-        products.forEach((product)=>{
-            productsHTML +=   `
+let productsHTML = "";
+products.forEach((product) => {
+  productsHTML += `
  <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -44,14 +13,14 @@
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars*10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceCents /100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -76,11 +45,40 @@
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+            product.id
+          }">
             Add to Cart
           </button>
         </div>
  `;
-})
+});
 
-document.querySelector(".js-products-grid").innerHTML=productsHTML;
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantiy").innerHTML = cartQuantity;
+  });
+});
